a4e6cb465b67b3d5edd436374a388d802b4154c8
T-Keerthika <keerthiglitter@gmail.com>
2023-02-21 12:15:56 +0530

    First commit

---
+++ demo/src/main/java/com/example/demo/controller/controller.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.pojoclass;
+
+
+@RestController
+public class controller {
+
+	@GetMapping("student")
+	public List<pojoclass> info(){
+		return Arrays.asList(new pojoclass(1,"keerthika","IT"),new pojoclass(2,"varna","CSE"));
+	}
+}
